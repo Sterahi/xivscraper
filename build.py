6a46f54b9b024fc49d7144a162c3bc50fd9b66df
lizmurray479@gmail.com <lizmurray479@gmail.com>
2016-11-03 20:00:35 -0400

    Currently working on internal iteration of weapons.

--- build.py
+++ build.py
@@ -13,10 +13,20 @@ class xivSpider(scrapy.Spider):
                 'name': xiv.css(NAME_SELECTOR).extract_first(),
                 'link': xiv.css(LINK_SELECTOR).extract_first(),
             }
-        NEXT_PAGE_SELECTOR ='.next a ::attr(href)'
+        NEXT_PAGE_SELECTOR = LINK_SELECTOR
         next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
         if next_page:
             yield scrapy.Request(
-                response.urljoin(next_page),
-                callback=self.parse
+            "http://na.finalfantasyxiv.com/" + next_page,
+                callback=self.parseStats
             )
+
+    def parseStats(self, response):
+        TABLE_SELECT = 'ul.db-view__basic_bonus'
+        STAT_SELECT = 'li li ::text'
+        VALUE_SELECT = 'li ::text'
+        for stat in response.css(TABLE_SELECT):
+            yield {
+                'stat1': stat.css(STAT_SELECT).extract_first(),
+                'stat1#':stat.css(VALUE_SELECT).extract_first(),
+            }
