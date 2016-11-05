8936a703bf39a12c2ba2541bbaf030dcb0af4c8d
Elizabeth Murray <lizmurray479@gmail.com>
2016-11-04 23:25:08 -0400

    All weapons are now grabbed and inserted into a queue to be used to grab the stats. Method for stats is reusable for all items. Need to find way to limit insertions to 50+ gear.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # xivscraper
-## Current Time
- ~1hr
+## Current Timeline
+ 8 Hours total.
+ 4 hours to live weapon data.
+ 1 hour from weapon data to all left side + shields.
 ## To Run
 > pip install scrapy
 > This may need to be run as sudo
--- build.py
+++ build.py
@@ -1,6 +1,9 @@
 import scrapy
+import collections
 
-class xivSpider(scrapy.Spider):
+linkqueue = collections.deque()
+
+class weaponSpider(scrapy.Spider):
     name = 'xiv_spider'
     start_urls = ['http://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=1']
 
@@ -13,20 +16,48 @@ class xivSpider(scrapy.Spider):
                 'name': xiv.css(NAME_SELECTOR).extract_first(),
                 'link': xiv.css(LINK_SELECTOR).extract_first(),
             }
-        NEXT_PAGE_SELECTOR = LINK_SELECTOR
+            linkqueue.append(xiv.css(LINK_SELECTOR).extract_first())
+        NEXT_PAGE_SELECTOR ='.next a ::attr(href)'
         next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
         if next_page:
             yield scrapy.Request(
-            "http://na.finalfantasyxiv.com/" + next_page,
-                callback=self.parseStats
+                response.urljoin(next_page),
+                callback=self.parse
+            )
+        elif len(linkqueue) >=1:
+            for link in list(linkqueue):
+                next_page = "http://na.finalfantasyxiv.com/" + str(linkqueue[0])
+                linkqueue.popleft()
+                yield scrapy.Request(
+                    response.urljoin(next_page),
+                    callback=self.parseStats
+                )
+            next_page = "http://na.finalfantasyxiv.com/lodestone/playguide/db/item/?category2=3"
+            yield scrapy.Request(
+                response.urljoin(next_page),
+                callback=self.parse
             )
 
     def parseStats(self, response):
         TABLE_SELECT = 'ul.db-view__basic_bonus'
-        STAT_SELECT = 'li li ::text'
-        VALUE_SELECT = 'li ::text'
+        NAME_SELECT = 'normalize-space(//div/div/div/div/div/div/h2/text())'
+        STAT1_SELECT = '//div/div/div/div/div/div/div/div/ul/li[1]/span/text()'
+        VALUE1_SELECT = '//div/div/div/div/div/div/ul/li[1]/text()'
+        STAT2_SELECT = '//div/div/div/div/div/div/div/div/ul/li[2]/span/text()'
+        VALUE2_SELECT = '//div/div/div/div/div/div/ul/li[2]/text()'
+        STAT3_SELECT = '//div/div/div/div/div/div/div/div/ul/li[3]/span/text()'
+        VALUE3_SELECT = '//div/div/div/div/div/div/ul/li[3]/text()'
+        STAT4_SELECT = '//div/div/div/div/div/div/div/div/ul/li[4]/span/text()'
+        VALUE4_SELECT = '//div/div/div/div/div/div/ul/li[4]/text()'
         for stat in response.css(TABLE_SELECT):
             yield {
-                'stat1': stat.css(STAT_SELECT).extract_first(),
-                'stat1#':stat.css(VALUE_SELECT).extract_first(),
+                'name': stat.xpath(NAME_SELECT).extract(),
+                'stat1': stat.xpath(STAT1_SELECT).extract_first(),
+                'stat1#':stat.xpath(VALUE1_SELECT).extract_first(),
+                'stat2': stat.xpath(STAT2_SELECT).extract_first(),
+                'stat2#':stat.xpath(VALUE2_SELECT).extract_first(),
+                'stat3': stat.xpath(STAT3_SELECT).extract_first(),
+                'stat3#':stat.xpath(VALUE3_SELECT).extract_first(),
+                'stat4': stat.xpath(STAT4_SELECT).extract_first(),
+                'stat4#':stat.xpath(VALUE4_SELECT).extract_first(),
             }
